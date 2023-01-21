5a5a05b7137aa6d40690732fd2b2aca5c1723b48
Seongil Park <zihado@gmail.com>
2023-01-22 01:41:38 +0900

    crud

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,14 +3,19 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@Controller
-public class ArticleController {
+import java.util.List;
 
+@Controller
+@Slf4j
+public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
 
@@ -21,15 +26,38 @@ public class ArticleController {
 
     @PostMapping("articles/create")
     public String createArticle(ArticleForm form) {
-        System.out.println(form.toString());
+//        System.out.println(form.toString());
+        log.info(form.toString());
 
         // 1. Dto to Entity
         Article article = form.toEntity();
-        System.out.println(article.toString());
+//        System.out.println(article.toString());
+        log.info(article.toString());
 
         // 2. Repo to DB
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
-        return "";
+//        System.out.println(saved.toString());
+        log.info(saved.toString());
+        return "redirect:/articles/" + saved.getId();
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id = " + id);
+        // 1. data from id
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        // 2. data to model
+        model.addAttribute("article", articleEntity);
+
+        // 3. page setup
+        return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model) {
+        List<Article> articleEntityList = articleRepository.findAll();
+        model.addAttribute("articleList",articleEntityList);
+        return "articles/index";
     }
 }
--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -4,8 +4,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
+@AllArgsConstructor
+@ToString
+@NoArgsConstructor
+@Getter
 public class Article {
 
     @Id
@@ -18,18 +26,4 @@ public class Article {
     @Column
     private String content;
 
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
